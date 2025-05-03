4c933d1385fcb1b04715d97872ea6eedb13a8a13
Lucas Alves <lucasalves.57@hotmail.com>
2025-05-03 09:45:00 -0300

    refactor: movendo getDefaultPaginationControl para constantes

--- src/app/shared/constants/pagination.ts
+++ src/app/shared/constants/pagination.ts
@@ -1,3 +1,13 @@
+import { PaginationControl } from '../../features/cozinha/models/controlePaginacao';
+
 export const PAGINATION_DEFAULT_ITEMS_PER_PAGE_OPTIONS = [5, 10, 20, 50, 100];
 export const PAGINATION_DEFAULT_SIZE = 10;
 export const PAGINATION_DEFAULT_PAGE = 0;
+
+export function getDefaultPaginationControl(): PaginationControl {
+  return {
+    itemsPerPageOptions: PAGINATION_DEFAULT_ITEMS_PER_PAGE_OPTIONS,
+    size: PAGINATION_DEFAULT_SIZE,
+    page: PAGINATION_DEFAULT_PAGE,
+  } as PaginationControl;
+}
