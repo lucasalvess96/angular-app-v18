67830128ed490795069d81741b418f753796921d
Lucas Alves <lucasalves.57@hotmail.com>
2025-05-02 23:05:36 -0300

    refactor: removendo arquivos não usados

--- src/app/Features/cozinha/models/combineSortPropertyAndOrder.ts
+++
@@ -1,9 +0,0 @@
-import { SortOrder } from './controlePaginacao';
-
-export const combineSortPropertyAndOrder = (property?: string, order?: SortOrder) => {
-  if (property && order && order.length > 0) {
-    return `${property},${order}`;
-  }
-
-  return property ?? '';
-};
