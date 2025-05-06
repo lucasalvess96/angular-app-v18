8424085e2f739e57ead149e94bd702020f24b816
Lucas Alves <lucasalves.57@hotmail.com>
2025-05-06 10:02:07 -0300

    refactor: alterando listagem de cozinhas

--- src/app/Features/cozinha/pages/cozinhas/cozinhas.component.ts
+++ src/app/Features/cozinha/pages/cozinhas/cozinhas.component.ts
@@ -1,12 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { HttpParams } from '@angular/common/http';
-import { Component, inject, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { MatPaginator, PageEvent } from '@angular/material/paginator';
-import { MatSort, Sort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
+import { PageEvent } from '@angular/material/paginator';
+import { Sort } from '@angular/material/sort';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { BehaviorSubject, debounceTime, distinctUntilChanged, map, Observable, of, switchMap, tap } from 'rxjs';
+import {
+  BehaviorSubject,
+  combineLatest,
+  debounceTime,
+  distinctUntilChanged,
+  finalize,
+  map,
+  Observable,
+  switchMap,
+  tap,
+} from 'rxjs';
 import { materialModules } from '../../../../shared/angular-material/material-modules';
 import { SpinnerComponentComponent } from '../../../../shared/components/spinner-component/spinner-component.component';
 import { TableComponentComponent } from '../../../../shared/components/table-component/table-component.component';
@@ -34,59 +43,66 @@ import { CozinhaService } from '../../services/cozinha.service';
 export class CozinhasComponent implements OnInit {
   columnHeaders: { [key: string]: string } = { id: 'ID', nome: 'Nome' };
   displayedColumns: string[] = ['id', 'nome'];
-  dataSource!: MatTableDataSource<Cozinha>;
-  dataSource$!: Observable<Cozinha[]>;
 
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
+  dataSource$!: Observable<Cozinha[]>;
 
   paginationControl = getDefaultPaginationControl();
   paginationChange$ = new BehaviorSubject<void>(undefined);
 
   searchControl = new FormControl('');
+  searchTerm$ = new BehaviorSubject<string>('');
 
-  loading: boolean = false;
+  loading = false;
+
+  message = 'Carregando informações...';
 
   private readonly cozinhaService = inject(CozinhaService);
+  private readonly changeDetectorRef = inject(ChangeDetectorRef);
+
+  private readonly LOAD_DELAY = 400;
 
   ngOnInit(): void {
-    this.dataSource = new MatTableDataSource<Cozinha>([]);
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+    this.searchControl.valueChanges.pipe(debounceTime(this.LOAD_DELAY), distinctUntilChanged()).subscribe((term) => {
+      this.searchTerm$.next(term ?? '');
+    });
 
-    this.list();
-    this.filter();
+    this.dataSource$ = this.createDataSource();
   }
 
-  list(): void {
-    this.dataSource$ = this.paginationChange$.pipe(
-      switchMap(() => this.cozinhaService.list(this.buildPaginationParams())),
+  createDataSource(): Observable<Cozinha[]> {
+    return combineLatest([this.paginationChange$, this.searchTerm$]).pipe(
+      switchMap(([_, term]) => {
+        this.loading = true;
+        this.changeDetectorRef.detectChanges();
+        return (term.trim() ? this.cozinhaService.search(term) : this.fetchCozinhas()).pipe(
+          finalize(() => {
+            this.loading = false;
+            this.changeDetectorRef.detectChanges();
+          }),
+        );
+      }),
+    );
+  }
+
+  fetchCozinhas(): Observable<Cozinha[]> {
+    return this.cozinhaService.list(this.buildPaginationParams()).pipe(
       tap((response: Paginacao) => {
-        this.paginationControl = {
-          ...this.paginationControl,
-          totalElements: response.totalElements,
-        };
+        this.paginationControl.totalElements = response.totalElements;
       }),
       map((response: Paginacao) => response.content),
     );
   }
 
-  filter(): void {
-    this.searchControl.valueChanges
-      .pipe(
-        debounceTime(300),
-        distinctUntilChanged(),
-        switchMap((term) => {
-          if (!term || term.trim() === '') {
-            return this.dataSource$;
-          }
+  buildPaginationParams(): HttpParams {
+    const { page, size, sortProperty, sortOrder } = this.paginationControl;
 
-          return this.cozinhaService.search(term);
-        }),
-      )
-      .subscribe((result: Cozinha[]) => {
-        this.dataSource$ = of(result);
-      });
+    let params = new HttpParams().set('size', size.toString()).set('page', page.toString());
+
+    if (sortProperty) {
+      params = params.set('sort', `${sortProperty},${sortOrder}`);
+    }
+
+    return params;
   }
 
   onPageChange(event: PageEvent): void {
@@ -99,17 +115,5 @@ export class CozinhasComponent implements OnInit {
     this.paginationControl.sortProperty = sort.active;
     this.paginationControl.sortOrder = sort.direction as SortOrder;
     this.paginationChange$.next();
-  }
-
-  private buildPaginationParams(): HttpParams {
-    const { page, size, sortProperty, sortOrder } = this.paginationControl;
-
-    let params = new HttpParams().set('size', size.toString()).set('page', page.toString());
-
-    if (sortProperty) {
-      params = params.set('sort', `${sortProperty},${sortOrder}`);
-    }
-
-    return params;
   }
 }
