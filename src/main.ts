6cb9d6e921f13a9303e87afa765225fb594cf313
Lucas Alves <lucasalves.57@hotmail.com>
2025-05-16 15:54:07 -0300

    feat: pipe currency br

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
+import { registerLocaleData } from '@angular/common';
 import { provideHttpClient } from '@angular/common/http';
+import localePt from '@angular/common/locales/pt';
+import { LOCALE_ID } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
+registerLocaleData(localePt, 'pt-BR');
+
 bootstrapApplication(AppComponent, {
   ...appConfig,
-  providers: [...(appConfig.providers ?? []), provideHttpClient(), provideAnimations()],
+  providers: [
+    ...(appConfig.providers ?? []),
+    provideHttpClient(),
+    provideAnimations(),
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+  ],
 }).catch((err) => console.error(err));
