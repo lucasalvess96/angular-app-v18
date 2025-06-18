6a6046ad979bf802ea97b24ef6256ad477b763ce
Lucas Alves <lucasalves.57@hotmail.com>
2025-06-18 00:47:18 -0300

    feat: componente de login

---
+++ src/app/Features/login/pages/login/login.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { materialModules } from '../../../../shared/angular-material/material-modules';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive, materialModules, CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent implements OnInit {
+  private readonly route = inject(ActivatedRoute);
+  private readonly router = inject(Router);
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      const token = params['token'];
+      if (token) {
+        localStorage.setItem('token', token);
+        const redirectUrl = localStorage.getItem('redirectAfterLogin') ?? '/restaurantes';
+        this.router.navigate([redirectUrl]);
+      }
+    });
+  }
+
+  loginWithGoogle(): void {
+    localStorage.setItem('redirectAfterLogin', this.router.url);
+    window.location.href = 'http://localhost:8080/oauth2/authorization/google';
+  }
+}
