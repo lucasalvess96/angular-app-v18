5cc30d4227fbf26c99e576edc1a7dc65dbf592c5
Lucas Alves <lucasalves.57@hotmail.com>
2025-06-18 00:49:37 -0300

    refactor: excecao service

--- src/app/shared/services/excecao.service.ts
+++ src/app/shared/services/excecao.service.ts
@@ -1,12 +1,14 @@
+import { Location } from '@angular/common';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ExcecaoService {
-  constructor() {}
+  constructor(private readonly location: Location, private readonly router: Router) {}
 
   public handleError(error: HttpErrorResponse): Observable<never> {
     if (error.status === 0) {
@@ -14,6 +16,13 @@ export class ExcecaoService {
     } else {
       alert('erro interno no servidor');
     }
+
+    if (history.length > 1) {
+      this.location.back();
+    } else {
+      this.router.navigate(['../home']);
+    }
+
     throw new Error('Não foi possivél se conectar ao backend');
   }
 }
