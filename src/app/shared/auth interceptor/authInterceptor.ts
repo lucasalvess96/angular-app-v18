823ef35574f77488249a668a23b4dc02bde88372
Lucas Alves <lucasalves.57@hotmail.com>
2025-06-18 00:50:07 -0300

    feat: interceptor token

---
+++ src/app/shared/auth interceptor/authInterceptor.ts
@@ -0,0 +1,21 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const isApiUrl = req.url.startsWith('http://localhost:8080');
+    const token = localStorage.getItem('token');
+
+    if (token && isApiUrl) {
+      const cloned = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return next.handle(cloned);
+    }
+    return next.handle(req);
+  }
+}
