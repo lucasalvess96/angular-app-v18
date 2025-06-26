1eaf1630c4fbd767e20ddaabdcd38281007ba308
Lucas Alves <lucasalves.57@hotmail.com>
2025-06-26 11:44:29 -0300

    refactor: movendo métodos onPageChange e onSortChange evitando duplicação

--- src/app/shared/components/table-component/table-component.component.ts
+++ src/app/shared/components/table-component/table-component.component.ts
@@ -4,6 +4,7 @@ import { PageEvent } from '@angular/material/paginator';
 import { Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { materialModules } from '../../angular-material/material-modules';
+import { PaginationControl, SortOrder } from '../../models/controlePaginacao';
 
 @Component({
   selector: 'app-table-component',
@@ -17,14 +18,21 @@ export class TableComponentComponent {
   @Input() columnHeaders: { [key: string]: string } = {};
   @Input() displayedColumns: string[] = [];
   @Input() actions?: TemplateRef<any>;
-  @Input() paginationControl!: {
-    page: number;
-    size: number;
-    totalElements: number;
-    itemsPerPageOptions?: number[];
-  };
+  @Input() paginationControl!: PaginationControl;
   @Input() customTemplates: { [key: string]: TemplateRef<any> } = {};
 
-  @Output() sortChange = new EventEmitter<Sort>();
   @Output() pageChange = new EventEmitter<PageEvent>();
+  @Output() sortChange = new EventEmitter<Sort>();
+
+  onPageChange(event: PageEvent): void {
+    this.paginationControl.page = event.pageIndex;
+    this.paginationControl.size = event.pageSize;
+    this.pageChange.emit(event);
+  }
+
+  onSortChange(sort: Sort): void {
+    this.paginationControl.sortProperty = sort.active;
+    this.paginationControl.sortOrder = sort.direction as SortOrder;
+    this.sortChange.emit(sort);
+  }
 }
