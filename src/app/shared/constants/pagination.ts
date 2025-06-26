ff95b38e3c68fc5828fcf396bd63ecb7196227f6
Lucas Alves <lucasalves.57@hotmail.com>
2025-06-26 11:43:41 -0300

    refactor: movendo método buildPaginationParams para reutilizacao

--- src/app/shared/constants/pagination.ts
+++ src/app/shared/constants/pagination.ts
@@ -1,3 +1,4 @@
+import { HttpParams } from '@angular/common/http';
 import { PaginationControl } from '../models/controlePaginacao';
 
 export const PAGINATION_DEFAULT_ITEMS_PER_PAGE_OPTIONS = [5, 10, 20, 50, 100];
@@ -10,4 +11,16 @@ export function getDefaultPaginationControl(): PaginationControl {
     size: PAGINATION_DEFAULT_SIZE,
     page: PAGINATION_DEFAULT_PAGE,
   } as PaginationControl;
+}
+
+export function buildPaginationParams(pagination: PaginationControl): HttpParams {
+  const { page, size, sortProperty, sortOrder } = pagination;
+
+  let params = new HttpParams().set('size', size.toString()).set('page', page.toString());
+
+  if (sortProperty) {
+    params = params.set('sort', `${sortProperty},${sortOrder}`);
+  }
+
+  return params;
 }
