ba301beab9958e5bf3e9ad6548a5e16d5e3522d2
Lucas Alves <lucasalves.57@hotmail.com>
2025-06-26 22:38:21 -0300

    feat: implementando modulos

--- src/app/shared/angular-material/material-modules.ts
+++ src/app/shared/angular-material/material-modules.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -18,4 +19,5 @@ export const materialModules = [
   MatTableModule,
   MatSortModule,
   MatPaginatorModule,
+  FormsModule,
 ];
