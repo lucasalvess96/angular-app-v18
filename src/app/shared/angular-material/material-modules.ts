3c2cf82bfe4a4fde33d975bf5308e37a83fcac4d
Lucas Alves <lucasalves.57@hotmail.com>
2025-09-15 23:11:04 -0300

    feat: material modules

--- src/app/shared/angular-material/material-modules.ts
+++ src/app/shared/angular-material/material-modules.ts
@@ -1,4 +1,4 @@
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -19,5 +19,6 @@ export const materialModules = [
   MatTableModule,
   MatSortModule,
   MatPaginatorModule,
+  ReactiveFormsModule,
   FormsModule,
 ];
