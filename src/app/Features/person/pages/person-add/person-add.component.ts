9a130715e6b2499d9e0afddfafba3142283141d5
Lucas Alves <lucasalves.57@hotmail.com>
2025-09-15 23:10:34 -0300

    refactor: person-add componente

--- src/app/Features/person/pages/person-add/person-add.component.ts
+++ src/app/Features/person/pages/person-add/person-add.component.ts
@@ -1,16 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { NgxMaskDirective } from 'ngx-mask';
+import { ToastrService } from 'ngx-toastr';
 import { materialModules } from '../../../../shared/angular-material/material-modules';
-import { SpinnerComponentComponent } from '../../../../shared/components/spinner-component/spinner-component.component';
-import { Person } from '../../models/person';
+import { DialogConfirmComponent } from '../../components/dialog-confirm/dialog-confirm.component';
 import { PersonService } from '../../services/person.service';
 
 @Component({
   selector: 'app-person-add',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, materialModules, CommonModule, SpinnerComponentComponent],
+  imports: [RouterLink, RouterLinkActive, materialModules, CommonModule, NgxMaskDirective],
   templateUrl: './person-add.component.html',
   styleUrl: './person-add.component.scss',
 })
@@ -22,6 +24,9 @@ export class PersonAddComponent implements OnInit {
   private readonly router = inject(Router);
 
   private readonly personService = inject(PersonService);
+  private readonly toastService = inject(ToastrService);
+
+  private readonly dialog = inject(MatDialog);
 
   ngOnInit(): void {
     this.createForm();
@@ -41,8 +46,8 @@ export class PersonAddComponent implements OnInit {
   onSubmit(): void {
     if (this.formGroup) {
       this.personService.create(this.formGroup.value).subscribe({
-        next: (response: Person) => {
-          console.log(response);
+        next: () => {
+          this.toastService.success('Cadastro realizado com sucesso!');
           this.formGroup?.reset();
           this.formGroup?.markAsUntouched();
           this.formGroup?.markAsPristine();
@@ -50,5 +55,20 @@ export class PersonAddComponent implements OnInit {
         },
       });
     }
+  }
+
+  openDialogConfirm(): void {
+    this.dialog
+      .open(DialogConfirmComponent, {
+        width: '30vw',
+        disableClose: true,
+        data: { formGroup: this.formGroup },
+      })
+      .afterClosed()
+      .subscribe((confirmed: boolean) => {
+        if (confirmed) {
+          this.onSubmit();
+        }
+      });
   }
 }
