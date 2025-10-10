98350388dbf231afcefdf40c54cb5600e69777e0
Lucas Alves <lucasalves.57@hotmail.com>
2025-10-09 22:35:23 -0300

    refactor: interface paginação

--- src/app/shared/models/paginacao.ts
+++ src/app/shared/models/paginacao.ts
@@ -1,34 +1,9 @@
 export interface Paginacao<T> {
   content: T[];
-  pageable: Pageable;
-  totalPages: number;
-  totalElements: number;
-  last: boolean;
-  numberOfElements: number;
-  first: boolean;
   size: number;
   number: number;
-  sort: Sort2;
-  empty: boolean;
-}
-
-export interface Pageable {
-  pageNumber: number;
-  pageSize: number;
-  sort: Sort;
-  offset: number;
-  paged: boolean;
-  unpaged: boolean;
-}
-
-export interface Sort {
-  sorted: boolean;
-  unsorted: boolean;
-  empty: boolean;
-}
-
-export interface Sort2 {
-  sorted: boolean;
-  unsorted: boolean;
-  empty: boolean;
+  totalElements: number;
+  totalPages: number;
+  first: boolean;
+  last: boolean;
 }
