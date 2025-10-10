1ebd9fc54ce651123acda95620fe19b4f437fe02
Lucas Alves <lucasalves.57@hotmail.com>
2025-10-10 08:18:24 -0300

    feat: providers paginação pt-br

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatPaginatorIntl } from '@angular/material/paginator';
 import { RouterOutlet } from '@angular/router';
+import { getPortuguesePaginatorIntl } from './shared/constants/portuguesePaginatorIntl';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
+  providers: [{ provide: MatPaginatorIntl, useValue: getPortuguesePaginatorIntl() }],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
